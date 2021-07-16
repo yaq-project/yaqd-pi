9ba96517ce2d7f95d7ec641bb93dc4dfe40b926b
lab <wright_stuff_chem@wisc.edu>
2021-07-16 10:31:07 -0500

    test proem daemon

--- yaqd_pi/_pi_proem.py
+++ yaqd_pi/_pi_proem.py
@@ -7,38 +7,40 @@ import numpy as np
 from time import sleep
 from yaqd_core import IsDaemon, IsSensor, HasMeasureTrigger, HasMapping
 from typing import Dict, Any, List
-from instrumental.drivers.cameras import picam as sdk, PicamEnums, list_instruments, PicamError
+from instrumental.drivers.cameras.picam import sdk, PicamEnums, list_instruments, PicamError
+
 
 class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
     _kind = "pi-proem"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        self._channel_names = ["image", "spectral_image"] 
-        # Putting in spectral_image as a channel here to denote when the
-        # prism is inserted into the beam line. Can then get xaxis readouts in 
-        # color units instead of pixel units
-        self._channel_mappings = {"image": ["x_index", "y_index"],
-                                  "spectral_image": ["wm", "y_index"]}
+        self.logger.debug("Hello, I am initializieng")
+        self._channel_names = ["image"]
+        self._channel_mappings = {"image": ["x_index", "y_index"]}
         self._mapping_units = {"x_index": "None", "y_index": "None", 
-                               "wm": ["eV", "nm", "um"]}
-        self._channel_units = {"image": "counts", "spectral_image": "counts"}        
+                               "wm": ["eV", "nm", "um"]}# this threw an error
+        # when calling c.get_mapping_units()
+        self._channel_units = {"image": "counts"}        
+        self._channel_shapes = {"image": (512,512)}
         self.picam = sdk
   
         # find devices
-        deviceArray = self.picam.list_instruments()
+        deviceArray = list_instruments()
         if len(deviceArray) == 0:
             raise PicamError("No devices found.")
             
         # create sdk.PicamCamera() object
-        self.proem = list_instruments()[0].create()
+        self.proem = deviceArray[0].create()
             
         # Perform any unique initialization
-        self.proem.params.AdcAnalogGain.set_value(getattr(PicamEnums.AdcAnalogGain), self._config["adc_analog_gain"])
+        self.proem.params.AdcAnalogGain.set_value(getattr(PicamEnums.AdcAnalogGain, self._config["adc_analog_gain"]))
         self.proem.params.AdcEMGain.set_value(self._config["em_gain"])
         self.proem.params.ExposureTime.set_value(self._config["exposure_time"])
         
-        #roi is currently in config, so just run on startup
+        # roi is currently in config, so just run on startup
+        # should eventually be able to change roi dynamically though to avoid
+        # having to restart daemon every time the roi needs to be changed.
         self._set_roi()
         self._set_temperature()
         
@@ -52,24 +54,8 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
         max_width = self.proem.params.SensorActiveWidth.get_value()
         max_height = self.proem.params.SensorActiveHeight.get_value()
         
-        # handle defaults
-        if left is None:
-            left = 1
-        if top is None:
-            top = 1
-        if width is None:
-            width = max_width - left + 1
-        if height is None:
-            height = max_height - top + 1
-        
         # make sure roi falls somewhere on sensor
         self.logger.debug(f"{max_width}, {max_height}, {x_binning}, {y_binning}, {width}, {left}, {height}, {top}")
-        w_extent = width * x_binning + (left-1)
-        h_extent = height * y_binning + (top-1)
-        if w_extent > max_width:
-            raise ValueError(f"width extends over {w_extent}, max is {max_width}")
-        if h_extent > max_height:
-            raise ValueError(f"height extends over {h_extent}, max is {max_height}")
         
         # finally set roi through instrumental function
         self.proem.set_roi(x=left, width=width, height=height, y=top,
@@ -77,7 +63,7 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
         
         
     def _set_temperature(self):
-        self.proem.params.SensorTemperatureSetpoint.set_value(self._config["sensor_temperature_setpoint"])
+        self.proem.params.SensorTemperatureSetPoint.set_value(self._config["sensor_temperature_setpoint"])
         sensor_temp_status = self.proem.params.SensorTemperatureStatus.get_value().name
         if sensor_temp_status == 'Locked':
             self.logger.info("Sensor temp stabilized.")
@@ -92,18 +78,16 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
             self.logger.info(f"Sensor is cooling.\
                              Target: {set_temp} C. Current: {sensor_temp} C.")
             sleep(5)
-            set_temp = self.proem.params.SensorTemperatureSetpoint.get_value() # call again just to make sure
+            set_temp = self.proem.params.SensorTemperatureSetPoint.get_value() # call again just to make sure
             sensor_temp = self.proem.params.SensorTemperatureReading.get_value()
             diff = set_temp - sensor_temp
         sensor_temp_status = self.proem.params.SensorTemperatureStatus().name
         if sensor_temp_status == 'Locked':
             self.logger.info("Sensor temp stabilized.")
        
-    def acquire_and_show(self):
-        # putting this simple function here now, can increase complexity as we see fit
-        from matplotlib.pyplot import imshow
-        data = self.proem.grab_image()
-        imshow(data)
+    async def _measure(self):
+            print(self.proem.grab_image().shape)
+            return {"image": self.proem.grab_image()}
        
     # def start_live_Video(self):
     #     self.proem.start_live_video()
