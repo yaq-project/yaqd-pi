68c7693a2875f98221105f1507bd64f9b1e46f9b
jscheeler1 <45218489+jscheeler1@users.noreply.github.com>
2021-10-29 17:27:42 -0500

    add get and set functions

--- yaqd_pi/_pi_proem.py
+++ yaqd_pi/_pi_proem.py
@@ -15,15 +15,15 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        self.logger.debug("Hello, I am initializieng")
+        if config.get("emulate"):
+            self.logger.debug("Starting Emulated camera")
+            sdk.connect_demo_camera(PicamEnums.Model.ProEMHS512BExcelon, "demo")
         self._channel_names = ["image"]
         self._channel_mappings = {"image": ["x_index", "y_index"]}
         self._mapping_units = {"x_index": "None", "y_index": "None", 
-                               "wm": ["eV", "nm", "um"]}# this threw an error
-        # when calling c.get_mapping_units()
+                               "wm": ["eV", "nm", "um"]} # this threw an error when calling c.get_mapping_units()
         self._channel_units = {"image": "counts"}        
         self._channel_shapes = {"image": (512,512)}
-        self.picam = sdk
   
         # find devices
         deviceArray = list_instruments()
@@ -32,36 +32,46 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
             
         # create sdk.PicamCamera() object
         self.proem = deviceArray[0].create()
-            
-        # Perform any unique initialization
-        self.proem.params.AdcAnalogGain.set_value(getattr(PicamEnums.AdcAnalogGain, self._config["adc_analog_gain"]))
-        self.proem.params.AdcEMGain.set_value(self._config["em_gain"])
-        self.proem.params.ExposureTime.set_value(self._config["exposure_time"])
         
-        # roi is currently in config, so just run on startup
-        # should eventually be able to change roi dynamically though to avoid
-        # having to restart daemon every time the roi needs to be changed.
-        self._set_roi()
         self._set_temperature()
         
-    def _set_roi(self):
-	#extract roi parameters from config
-        roi_keys = ["roi_x_binning", "roi_y_binning", "roi_width",
-                    "roi_left", "roi_height", "roi_top"]
-        x_binning, y_binning, width, left, height, top = [
-            self._config[k] for k in roi_keys]
+    def set_roi(self, roi):
+        try:
+            self.proem.set_roi(x=roi["left"], y=roi["top"],width=roi["width"],height=roi["height"],
+                               x_binning=roi["x_binning"],
+                               y_binning=roi["y_binning"])
+        except Exception as e:
+            print(e)
+        pld_roi = self.proem.params.Rois.get_value()[0]
+        self._state["roi"] = {"left":pld_roi.x, "width": pld_roi.width,
+                              "top":pld_roi.y, "height":pld_roi.height,
+                              "x_binning":pld_roi.x_binning,
+                              "y_binning":pld_roi.y_binning}
+    
+    def get_roi(self):
+        return self._state["roi"]
+    
+    def set_em_gain(self, em_gain: int):
+        self.proem.params.AdcEMGain.set_value(em_gain)
+        self._state["em_gain"] = self.proem.params.AdcEMGain.get_value()     
         
-        max_width = self.proem.params.SensorActiveWidth.get_value()
-        max_height = self.proem.params.SensorActiveHeight.get_value()
+    def get_em_gain(self):
+        return self._state["em_gain"]
+    
+    def set_exposure_time(self, exposure_time: float):
+        self.proem.params.ExposureTime.set_value(exposure_time)
+        self._state["exposure_time"] = self.proem.params.ExposureTime.get_value()
         
-        # make sure roi falls somewhere on sensor
-        self.logger.debug(f"{max_width}, {max_height}, {x_binning}, {y_binning}, {width}, {left}, {height}, {top}")
+    def get_exposure_time(self):
+        return self._state["exposure_time"]
+    
+    def set_adc_analog_gain(self, gain: str):
+        self.proem.params.AdcAnalogGain.set_value(getattr(PicamEnums.AdcAnalogGain, gain))
+        self._state["adc_analog_gain"] = self.proem.params.AdcAnalogGain.get_value().name
         
-        # finally set roi through instrumental function
-        self.proem.set_roi(x=left, width=width, height=height, y=top,
-			   x_binning=x_binning, y_binning=y_binning)
-        
-        
+    def get_adc_analog_gain(self):
+        return self._state["adc_analog_gain"]
+    
     def _set_temperature(self):
         self.proem.params.SensorTemperatureSetPoint.set_value(self._config["sensor_temperature_setpoint"])
         sensor_temp_status = self.proem.params.SensorTemperatureStatus.get_value().name
@@ -77,32 +87,31 @@ class PiProem(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
         while abs(diff) > 0.2:
             self.logger.info(f"Sensor is cooling.\
                              Target: {set_temp} C. Current: {sensor_temp} C.")
-            sleep(5)
             set_temp = self.proem.params.SensorTemperatureSetPoint.get_value() # call again just to make sure
             sensor_temp = self.proem.params.SensorTemperatureReading.get_value()
             diff = set_temp - sensor_temp
-        sensor_temp_status = self.proem.params.SensorTemperatureStatus().name
-        if sensor_temp_status == 'Locked':
-            self.logger.info("Sensor temp stabilized.")
-       
+        sensor_temp_status = self.proem.params.SensorTemperatureStatus().name       
+            
+    def get_sensor_temperature(self):
+        return self.proem.params.SensorTemperatureReading.get_value()
+    
+    def close(self):
+        self.proem.close()
+        
     async def _measure(self):
-            print(self.proem.grab_image().shape)
-            return {"image": self.proem.grab_image()}
-       
-    # def start_live_Video(self):
-    #     self.proem.start_live_video()
-
-    # def stop_live_Video(self):
-    #     self.proem.stop_live_video()
+        img = self.proem.grab_image()
+        print(img.shape)
+        return {"image": img}
            
-    # async def update_state(self):
-    #     """Continually monitor and update the current daemon state."""
-    #     # If there is no state to monitor continuously, delete this function
-    #     while True:
-    #         # Perform any updates to internal state
-    #         self._busy = False
-    #         # There must be at least one `await` in this loop
-    #         # This one waits for something to trigger the "busy" state
-    #         # (Setting `self._busy = True)
-    #         # Otherwise, you can simply `await asyncio.sleep(0.01)`
-    #         await self._busy_sig.wait()
+    async def update_state(self):
+        """Continually monitor and update the current daemon state."""
+        while True:
+            sensor_temp_status = self.proem.params.SensorTemperatureStatus.get_value().name
+            if sensor_temp_status == 'Locked':
+                self.logger.debug("Sensor temp stabilized.")
+            else:
+                self.logger.error("Sensor temp not stabilized. Something is wrong.")
+            await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    PiProem.main()
